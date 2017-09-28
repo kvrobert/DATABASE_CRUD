dc13b38a3f66dfbfdfbef41916d6411d44a3bfa5
robesz <kvrobert@gmail.com>
2017-09-28 17:49:19 +0200

    Adding InsertData class

---
+++ src/java/Model/Controller/InsertData.java
@@ -0,0 +1,96 @@
+
+package Model.Controller;
+
+import Model.DatabaseConnection;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class InsertData extends HttpServlet {
+    
+    String FIRSTNAME, LASTNAME, POSITION, SALARY;
+    String QUERY;
+    Connection conn;
+    ResultSet res;
+    Statement stmt;
+    DatabaseConnection dbconn;
+    
+
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            FIRSTNAME = request.getParameter("firstname");
+            LASTNAME = request.getParameter("lastname");
+            POSITION = request.getParameter("position");
+            SALARY = request.getParameter("salary"); 
+            
+            dbconn = new DatabaseConnection();
+            conn = dbconn.setConnection();
+            stmt = conn.createStatement();
+            
+            QUERY = "INSERT INTO dolgozok (firstname, lastname, positio, salary)values('"+FIRSTNAME+"', '"+LASTNAME+"', '"+POSITION+"', '"+SALARY+"') ";
+            int i = stmt.executeUpdate(QUERY);
+            
+             
+                    
+        }catch(Exception e)
+        {
+            request.setAttribute("Error", e);
+            RequestDispatcher rd = request.getRequestDispatcher("error.jsp");
+            rd.forward(request, response);
+        }finally
+        {
+            RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+            rd.forward(request, response);
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
